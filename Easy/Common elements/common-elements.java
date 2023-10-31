afe397a7a92abeee0017586122c36d608dec6d81
Ganesh Patil <122199481+ganeshPatil0000@users.noreply.github.com>
2023-10-31 23:31:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Common elements/common-elements.java
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            
+            int A[] = new int[n1];
+            int B[] = new int[n2];
+            int C[] = new int[n3];
+            
+            for (int i = 0;i < n1;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n2;i++)
+            {
+                B[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n3;i++)
+            {
+                C[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.commonElements(A, B, C, n1, n2, n3);
+            if (res.size() == 0)
+            {
+                System.out.print(-1);
+            }
+            else 
+            {
+                for (int i = 0;i < res.size();i++)
+                {
+                    System.out.print(res.get(i) + " ");
+                }    
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+        // code here 
+        int i = 0, j = 0, k = 0;
+        ArrayList<Integer> res = new ArrayList();
+        //Use a set collection framework for filtering duplicate
+        Set<Integer> seen = new HashSet();
+        
+        //iterates all the array 
+        while(i<n1 && j<n2 && k<n3){
+            //Check whether all elements are equal or not
+            if(A[i]==B[j] && B[j]==C[k]){
+                //use set 
+                if(seen.add(A[i])){
+                    res.add(A[i]);
+                }
+                i++;
+                j++;
+                k++;
+                continue;
+            }
+            //iterate each individual pointer to check the condition
+            else if(B[j]<C[k]){
+                j++;
+            }
+            else if(A[i]<B[j]){
+                i++;
+            }
+            else{
+                k++;
+            }
+        }
+        return res;
+        
+        //
+    }
+}
