8fc8b4af4fa309da6724881423c77df34a772ba1
Ganesh Patil <122199481+ganeshPatil0000@users.noreply.github.com>
2023-11-15 23:55:51 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Taking input - Java/taking-input-java.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Geeks{
+    
+    // Function to take input using Scanner class
+    static void IOFunction(){
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    
+		    // Your code here
+		    
+		    int a=sc.nextInt();
+          System.out.println(a);
+           float b=sc.nextFloat();
+           System.out.println(b);
+          long c=sc.nextLong();
+          System.out.println(c);
+          byte d=sc.nextByte();
+          System.out.println(d);
+          String s=new String("geeks for geeks courses");
+          System.out.println(s);     
+		}
+		
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+
+class GFG {
+    
+    // Driver Code
+    public static void main (String[] args) {
+	    
+	       //Creating an object of class Geeks
+	       Geeks g = new Geeks();
+		
+		   //Calling the IOFunction() of class Geeks
+		   g.IOFunction();
+	}
+}
+// } Driver Code Ends
